00ee063a68e50109087533358a648490538e7e87
Ian Létourneau <letourneau.ian@gmail.com>
2018-03-13 21:08:37 -0400

    Implement route to fetch list of all user stories (#1)

---
+++ server/api/user_story/user_story.controller.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const UserStoryRepository = require('../../persistence/mongo/user_story/user_story.repository')
+
+exports.index = async (req, res) => {
+    try {
+        const userStories = await UserStoryRepository.find()
+        console.log(userStories)
+        return res.status(200).json(userStories)
+    } catch (_) {
+        console.log(_)
+        return res.status(200).json([])
+    }
+}
